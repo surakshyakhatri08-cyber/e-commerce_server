bfb4441441a52a82478e808cd7b611d8e80eaa35
Surakshya Khatri <surakshyakhatri08@gmail.com>
2026-07-07 07:37:36 +0545

    add health route

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,15 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+//health route
+app.get('/', (req, res) => {
+    res.status(200).json({ 
+        message: "server is up and running",
+        status: "success",
+        data: null,
+    });
+});
+
 export default app;
