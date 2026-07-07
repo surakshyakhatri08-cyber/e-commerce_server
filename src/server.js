2af4b31673085e7f8c7391ddc14657a0e0208938
Surakshya Khatri <surakshyakhatri08@gmail.com>
2026-07-07 07:15:18 +0545

    Merge remote changes

--- src/server.js
+++ src/server.js
@@ -8,6 +8,6 @@ app.use(express.json());
 
 
 app.listen(8000, () => {
-    console.log("Server is running");
+    console.log("Server is running at http://localhost:8000");
     console.log("Press ctrl+c for close the server");
 });
