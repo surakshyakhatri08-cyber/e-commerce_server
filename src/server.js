e095d7075836450d859fe8820765bac2491fc656
Surakshya Khatri <surakshyakhatri08@gmail.com>
2026-07-07 07:05:23 +0545

    setup server

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+
+
+
+app.listen(8000, () => {
+    console.log("Server is running");
+    console.log("Press ctrl+c for close the server");
+});
